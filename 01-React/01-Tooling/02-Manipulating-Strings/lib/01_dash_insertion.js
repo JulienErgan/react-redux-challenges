333025177979fc53c54bf21a888b193b9d8ca306
Julien Ergan <julien.ergan.code@gmail.com>
2020-08-27 14:01:46 +0900

    dash insertion completed

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,20 @@
+function isConsonant(letter) {
+  // NB: The i modifier is used to perform case-insensitive matching.
+  return !letter.match(/[aeiouy\s]/i);
+}
+
+
 function insertDash(word) {
   // TODO: implement the method and return word with dashes
-  return null;
+  if (!word || word.length === 0) {
+    return "";
+  }
+  return word.split("").map((letter, index, previousLetter) => {
+    if (index > 0 && isConsonant(previousLetter[index-1]) && isConsonant(letter)) {
+      return `-${letter}`;
+    }
+    return letter;
+  }).join("");
 }
 
 module.exports = insertDash;
